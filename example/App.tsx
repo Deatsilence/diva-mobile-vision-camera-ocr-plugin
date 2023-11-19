6d3c94a885e9c6b803219e0844a06ba0be9cf091
FClementi <francesco.clementi1992@gmail.com>
2023-11-19 11:20:17 +0000

    Fixed android example

--- example/App.tsx
+++ example/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {LayoutChangeEvent, PixelRatio, SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {
   Camera,
   useCameraDevice,
@@ -8,13 +8,16 @@ import {
 } from 'react-native-vision-camera';
 import {Worklets} from 'react-native-worklets-core';
 import {OCRFrame, scanOCR} from 'vision-camera-ocr-plugin';
-
+console.log(OCRFrame);
 function App(): JSX.Element {
+  const [pixelRatio, setPixelRatio] = React.useState<number>(1);
+  const [ocr, setOcr] = React.useState<OCRFrame>();
   const {hasPermission, requestPermission} = useCameraPermission();
   const device = useCameraDevice('back');
 
   const onValueDetected = Worklets.createRunInJsFn((OCRFrame: OCRFrame) => {
     console.log(OCRFrame);
+    setOcr(OCRFrame);
   });
 
   const frameProcessor = useFrameProcessor(frame => {
@@ -29,6 +32,36 @@ function App(): JSX.Element {
     requestPermission();
   }, []);
 
+  const renderOverlay = () => {
+    return (
+      <>
+        {ocr?.result.blocks.map((block, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              style={{
+                position: 'absolute',
+                left: block.frame.x * pixelRatio,
+                top: block.frame.y * pixelRatio,
+                backgroundColor: 'white',
+                padding: 8,
+                borderRadius: 6,
+              }}>
+              <Text
+                style={{
+                  fontSize: 25,
+                  justifyContent: 'center',
+                  textAlign: 'center',
+                }}>
+                {block.text}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
+      </>
+    );
+  };
+
   if (device == null) return <Text>Device not available</Text>;
 
   return (
@@ -36,11 +69,21 @@ function App(): JSX.Element {
       <Camera
         style={[StyleSheet.absoluteFill]}
         frameProcessor={frameProcessor}
+        //style={{height: 600}}
         device={device}
         isActive={true}
         pixelFormat="yuv"
-        orientation={'portrait'}
+        onLayout={(event: LayoutChangeEvent) => {
+          setPixelRatio(
+            event.nativeEvent.layout.width /
+              PixelRatio.getPixelSizeForLayoutSize(
+                event.nativeEvent.layout.width,
+              ),
+          );
+        }}
+        //orientation={'portrait'}
       />
+      {renderOverlay()}
     </SafeAreaView>
   );
 }
